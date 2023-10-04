84b178c1bb490a38baad800b61906a863385f8c4
teddyirwn <tdyirawan0@gmail.com>
2023-10-04 20:40:53 +0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -17,6 +17,7 @@ window.onscroll = () => {
         header.classList.add('navbar-fixed')
 
 
+
     } else {
         header.classList.remove('navbar-fixed')
     }
@@ -46,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const darkMode = document.getElementById("darkMode");
     const html = document.querySelector('html')
 
-
+    const darkImage = document.getElementById('DarkImage');
     // Mengambil status dark mode dari penyimpanan lokal saat halaman dimuat
     const isDarkMode = localStorage.getItem("darkMode") === "true";
 
@@ -54,17 +55,44 @@ document.addEventListener("DOMContentLoaded", function () {
     // Mengubah tema sesuai dengan status dark mode yang tersimpan
     if (isDarkMode) {
         html.classList.add("dark");
+
     }
 
     // Menangani klik tombol untuk mengaktifkan/deaktifkan dark mode
     darkMode.addEventListener("click", function () {
-        html.classList.toggle("dark");
+        let darkm = html.classList.toggle("dark");
 
+        if (darkm) {
+            darkImage.src = "./image/sun.png"
+        } else {
+            darkImage.src = "./image/moon.png"
+        }
 
         // Menyimpan status dark mode ke penyimpanan lokal
         localStorage.setItem("darkMode", html.classList.contains("dark"));
+
+
     });
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark')
+        darkImage.src = "./image/sun.png"
+    } else {
+        document.documentElement.classList.remove('dark')
+        darkImage.src = "./image/moon.png"
+    }
 
 
 });
 
+
+
+
+
+// animation typing
+const typed = new Typed('#typing', {
+    strings: ["Frontend Developer", "Mobile Developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+
+})
